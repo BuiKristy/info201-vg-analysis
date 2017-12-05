e87dcf5be91c13638642916dfd6896e25bc8a357
Sindhooja Mullapudi <sindhoojamullapudi@gmail.com>
2017-12-04 22:32:56 -0800

    added another tab for summary with image

--- server.r
+++ server.r
@@ -12,6 +12,7 @@ source("scripts/chart2.R")
 # Define data based on input and turn into output plot using this server function
 shinyServer(function(input, output) {
   
+  
   YearRangeOfSalesPlot(input, output)
  RegionVsYear(input, output)
 })
--- ui.r
+++ ui.r
@@ -3,13 +3,40 @@
 # 11/19/17
 # This UI.R file will be the layout for the webpage that displays the charts.
 
-library(shiny)
-library(plotly)
+library("shiny")
+library("plotly")
+library("HSAUR")
+library("ggsci")
+
 
 my.ui <- navbarPage(
   
   # title of application
   "Video Game Sales Analysis",
+  tabPanel(
+    "Summary of our Analysis",
+    sidebarLayout(
+      sidebarPanel(
+        img(src="vgImage.jpg")
+        
+      ),
+      mainPanel(
+        br("We are working with sales data of more than 16,500 video games with over 100,000 sold copies over the last few years. 
+            We accessed this data from Kaggle. The data collected was published over a year ago."),
+        br("Our target audience is video game reviewers, video game players, and video game enthusiasts. 
+           But for our data we are focusing on video game reviewers. 
+           They may want to know which video game companies or genres have done better than other over the years, 
+           which platform was used the most to play the games, or which part of the world did better in sales depending on the game."),
+        p(strong("The three specific questions we decided to do analysis on and show visually were:")),
+        p(em("What are all the details of the video game given a particular rank?
+           What are the percent of sales for each genre of video game in a given year?
+           What are the total number of video game sales by each publisher in the given year and region?")),
+        br("For each question, there are tables and graphs represeting the analysis in the proceeding tabs.")
+        
+      )
+    )
+    
+  ),
   
   tabPanel(
     "Percentage of Sales Per Genre in Given Timeframe",
